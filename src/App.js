6e54d0bf3dd69d982a834cd11189695135d7d99b
Yakushisa <44353320+Yakushisa@users.noreply.github.com>
2018-12-30 00:25:43 -0200

    Refactoring component using bootstrap

--- src/App.js
+++ src/App.js
@@ -8,18 +8,39 @@ import Login from './Login';
 class App extends Component {
   render() {
     return (
-      <div>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/secret">Secret</Link></li>
-          <li><Link to="/login">Login</Link></li>
-        </ul>
-
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/secret" component={withAuth(Secret)} />
-          <Route path="/login" component={Login} />
-        </Switch>
+      <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a class="navbar-brand" href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/e/ea/Boostrap_logo.svg" width="30" height="30" alt="" /></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <Link class="nav-link" to="/">Home</Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link" to="/secret">Secret</Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link" to="/login">Login</Link>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <hr />
+        <div class="row">
+          <div class="col-sm-12">
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/secret" component={withAuth(Secret)} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </div>
+       </div>
       </div>
     );
   }
